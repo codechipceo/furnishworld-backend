9aaa4550440c0821eab76c2b81f40a061b7af69f
Mansab Mir <mansabmirsde@gmail.com>
2025-06-20 21:37:07 +0530

    Preview secret removed

--- src/app/(frontend)/next/preview/route.ts
+++ src/app/(frontend)/next/preview/route.ts
@@ -24,9 +24,7 @@ export async function GET(
   const slug = searchParams.get('slug')
   const previewSecret = searchParams.get('previewSecret')
 
-  if (previewSecret !== process.env.PREVIEW_SECRET) {
-    return new Response('You are not allowed to preview this page', { status: 403 })
-  }
+
 
   if (!path || !collection || !slug) {
     return new Response('Insufficient search params', { status: 404 })
